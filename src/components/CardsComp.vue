4c265552502827f0e2b45f2ecacfa245d4220378
Ddanilo9 <interiordanilo3@gmail.com>
2022-09-06 15:53:02 +0200

    cards components

---
+++ src/components/CardsComp.vue
@@ -0,0 +1,35 @@
+<template>
+   
+    <div class="cards">
+        <img :src="src" :alt="titolo">
+        <h4>{{titolo}}</h4>
+    </div>
+</template>
+    
+    
+  <script>
+    export default {
+        props: {
+            src: String,
+            titolo: String
+        }
+    }  
+  </script>
+    
+  
+
+<style scoped lang="scss">
+    .cards{
+        padding: 10px;
+        cursor: pointer;
+        
+        img{
+            aspect-ratio: 1/1;
+        }
+
+        h4{
+            font-size: 12px;
+            font-weight: 300;
+        }
+    }
+</style>
